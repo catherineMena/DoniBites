cdd40c8c06a13b5fb43d01fa169d0b884ebccc48
catherineMena <143752009+catherineMena@users.noreply.github.com>
2024-05-18 20:08:44 -0600

    avance en autentificacion de usuario

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { AuthenticationService } from '../services/authentication.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
+
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor(private authService: AuthenticationService,private router: Router) { }
+
+  onSubmit() {
+    this.authService.login({ nombre_usuario: this.username, contraseña: this.password }).subscribe(response => {
+      // handle successful login
+      this.router.navigate(['/menu']);
+      console.log('Login successful', response);
+    }, error => {
+      // handle error
+      console.error('Login failed', error);
+    });
+  }
 }
